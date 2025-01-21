a26fc19e2f8e78163d551455892b5a7f0965b79a
mesteranas <135442710+mesteranas@users.noreply.github.com>
2025-01-21 17:04:02 +0200

    added questions manager

--- full_marks/home/models.py
+++ full_marks/home/models.py
@@ -12,3 +12,20 @@ class notesContent(models.Model):
     category=models.ForeignKey(notesCategory,on_delete=models.CASCADE)
     def __str__(self):
         return self.name + " " + self.category.name
+class examSubject(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+class subjectCategory(models.Model):
+    name=models.CharField(max_length=200)
+    text=models.TextField()
+    subject=models.ForeignKey(examSubject,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name + "-" + self.subject.name
+class question(models.Model):
+    title=models.CharField(max_length=1000)
+    correctAnswer=models.CharField(max_length=1000)
+    otherAnswers=models.CharField(max_length=5000)
+    category=models.ForeignKey(subjectCategory,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title + " - " + self.category
--- full_marks/home/views.py
+++ full_marks/home/views.py
@@ -86,3 +86,90 @@ def deleteNoteCategory(r,categoryID:int):
         category.delete()
         return redirect("notesCategory")
     return render(r,"deleteCategory.html")
+@login_required
+def questionsManigerSubjects(r):
+    subjects=models.examSubject.objects.all().order_by("-name")
+    return render(r,"questionsManager/subject.html",{"subjects":subjects})
+@login_required
+def questionsManagerNewSubject(r):
+    if r.method=="POST":
+        data=r.POST
+        mdl=models.examSubject(name=data["name"])
+        mdl.save()
+        return redirect("questionsManigerSubjects")
+    return render(r,"questionsManager/newSubject.html")
+@login_required
+def questionsManagerCategory(r,subjectID:int):
+    subject=get_object_or_404(models.examSubject,pk=subjectID)
+    categories=models.subjectCategory.objects.filter(subject=subject).order_by("-name")
+    return render(r,"questionsManager/categories.html",{"subject":subject,"categories":categories})
+@login_required
+def questionsManagerNewCategory(r,subjectID):
+    subject=get_object_or_404(models.examSubject,pk=subjectID)
+    if r.method=="POST":
+        data=r.POST
+        mdl=models.subjectCategory(name=data["name"],text=data["text"],subject=subject)
+        mdl.save()
+        return redirect("questionsManagerCategory",subjectID=subjectID)
+    return render(r,"questionsManager/newCategory.html")
+@login_required
+def questionsManagerRenameSubject(r,subjectID:int):
+    subject=get_object_or_404(models.examSubject,pk=subjectID)
+    if r.method=="POST":
+        data=r.POST
+        subject.name=data["name"]
+        subject.save()
+        return redirect("questionsManagerCategory",subjectID=subjectID)
+    return render(r,"questionsManager/renameSubject.html",{"subject":subject})
+@login_required
+def questionsManagerDeleteSubject(r,subjectID:int):
+    if r.method=="POST":
+        mdl=get_object_or_404(models.examSubject,pk=subjectID)
+        mdl.delete()
+        return redirect("questionsManigerSubjects")
+    return render(r,"questionsManager/deleteSubject.html")
+@login_required
+def questionsManagerQuestions(r,subjectID:int,categoryID:int):
+    category=get_object_or_404(models.subjectCategory,pk=categoryID)
+    questions=models.question.objects.filter(category=category).order_by("-title")
+    return render(r,"questionsManager/questions.html",{"category":category,"questions":questions})
+@login_required
+def questionsManagerNewQuestion(r,subjectID:int,categoryID:int):
+    if r.method=="POST":
+        data=r.POST
+        category=get_object_or_404(models.subjectCategory,pk=categoryID)
+        mdl=models.question(title=data["title"],correctAnswer=data["correctAnswer"],otherAnswers=data["otherAnswers"],category=category)
+        mdl.save()
+        return redirect("questionsManagerQuestions",subjectID=subjectID,categoryID=categoryID)
+    return render(r,"questionsManager/newQuestion.html")
+@login_required
+def questionsManagerEditOrDeleteQuestion(r,subjectID:int,categoryID:int,questionID:int):
+    question=get_object_or_404(models.question,pk=questionID)
+    if r.method=="POST":
+        data=r.POST
+        if data["type"]=="edit":
+            question.title=data["title"]
+            question.correctAnswer=data["correctAnswer"]
+            question.otherAnswers=data["otherAnswers"]
+            question.save()
+        elif data["type"]=="delete":
+            question.delete()
+        return redirect("questionsManagerQuestions",subjectID=subjectID,categoryID=categoryID)
+    return render(r,"questionsManager/editQuestion.html",{"question":question})
+@login_required
+def questionsManagerDeleteCategory(r,subjectID:int,categoryID:int):
+    if r.method=="POST":
+        category=get_object_or_404(models.subjectCategory,pk=categoryID)
+        category.delete()
+        return redirect("questionsManagerCategory",subjectID=subjectID)
+    return render(r,"questionsManager/deleteCategory.html")
+@login_required
+def questionsManagerEditCategory(r,subjectID:int,categoryID:int):
+    category=get_object_or_404(models.subjectCategory,pk=categoryID)
+    if r.method=="POST":
+        data=r.POST
+        category.name=data["name"]
+        category.text=data["text"]
+        category.save()
+        return redirect("questionsManagerQuestions",subjectID=subjectID,categoryID=categoryID)
+    return render(r,"questionsManager/editCategory.html",{"category":category})
